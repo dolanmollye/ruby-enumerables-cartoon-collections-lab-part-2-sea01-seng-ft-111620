f2267beb683b33f468c5c3b0e15dadec15b4cecb
Molly Dolan <dolanmollye@gmail.com>
2020-11-07 19:38:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map {|element|
-    element[0].upcase + "element"}
+    element[0].upcase + }
 end
 
 def long_planeteer_calls(planeteer_calls)
